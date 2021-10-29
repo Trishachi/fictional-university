887ce4b882dedc8f1052796c384e6f2cfc4806c5
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-10-28 22:03:30 -0600

    Initial Project Pages Setup - WIP

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,12 @@
-This is my amazing custom theme
+<?php 
+    $names = array('Brad', 'John', 'Jane', 'Meowsalot', 'Barksalot');
+
+    $count = 0;
+
+    while($count < count($names)){
+        echo "<li>Hi, my name is $names[$count] </li>";
+        count++;
+    }
+
+?>
+
